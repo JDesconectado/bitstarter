1d9b97f81b1530382bc725e5bdf4a371aadcc240
Victor Muguerza <jdesconectado18@gmail.com>
2013-07-10 14:23:47 -0500

    Added way to read from index.html text

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var fileName = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var text = fs.readFileSync(fileName, 'utf-8');
+  var buffer = new Buffer(text);
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
